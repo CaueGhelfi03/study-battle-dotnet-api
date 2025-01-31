c2116c4a3331d997da8bfdd476a655bee99ba696
CaueGhelfi03 <caue.ghelfi@sptech.school>
2025-01-30 22:30:47 -0300

    Inserindo projeto e estrutura de arquivos com configuraçoes iniciais.

--- SistemaDeTarefas/Domain/DTOs/TaskDTO/TaskRequestDTO.cs
+++ SistemaDeTarefas/Domain/DTOs/TaskDTO/TaskRequestDTO.cs
@@ -6,7 +6,7 @@ namespace TaskSystem.Domain.DTOs.TaskDTO
     {
         public string TaskName { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
 
         public Enums.Status.TaskStatus Status { get; set; }
         public TaskPriority Priority { get; set; }
