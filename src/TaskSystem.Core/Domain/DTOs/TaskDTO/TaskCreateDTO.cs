41bdf37b43a30b2e42e870ff984e87a92dd46d2a
CaueGhelfi03 <caue.ghelfi@sptech.school>
2025-03-26 16:45:30 -0300

    feat: add new Tasks DTOs

--- src/TaskSystem.Core/Domain/DTOs/TaskDTO/TaskCreateDTO.cs
+++ src/TaskSystem.Core/Domain/DTOs/TaskDTO/TaskCreateDTO.cs
@@ -3,6 +3,8 @@ namespace TaskSystem.Core.Domain.DTOs.TaskDTO
 {
     public record TaskCreateDTO
     {
+        public Guid ChallengeId { get; set; }
+        public Guid UserId { get; set; }
         public string TaskName { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
