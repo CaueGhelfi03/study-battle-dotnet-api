61f23b5365ea033c4328c126c6bad01da3f134e5
CaueGhelfi03 <caue.ghelfi@sptech.school>
2025-05-20 15:05:03 -0300

    feat(dto): add subject field to ChallengeDTO

--- src/TaskSystem.Core/Domain/DTOs/ChallengeDTO/ChallengeResponseDTO.cs
+++ src/TaskSystem.Core/Domain/DTOs/ChallengeDTO/ChallengeResponseDTO.cs
@@ -9,6 +9,7 @@ namespace TaskSystem.Core.Domain.DTOs.ChallengeDTO
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Subject { get; set; }
         public int DurationDays { get; set; }
         public DateTime Start_Date { get; set; }
         public DateTime End_Date { get; set; }
