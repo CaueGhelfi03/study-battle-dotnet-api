3bf18c6379adc211caa8f8869e4eab94650e885e
CaueGhelfi03 <caue.ghelfi@sptech.school>
2025-07-01 20:50:48 -0300

    config: cloudinary configuration

---
+++ src/StudyBattle.API/Services/CloudinaryService/CloudinaryService.cs
@@ -0,0 +1,28 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace StudyBattle.API.Services.CloudinaryService
+{
+    public class CloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadTaskImageAsync(IFormFile file)
+        {
+            await using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = "Tasks"
+            };
+
+            var result = await _cloudinary.UploadAsync(uploadParams);
+            return result.SecureUrl.ToString();
+        }
+    }
+}
